5fd9c71d64b7ec68a21d36738bfe8d1c8e6ad9ab
HdA <gaborneubauer.hda@gmail.com>
2023-04-03 19:13:15 +0200

    ADD User mongoDB model

---
+++ backend/models/user.ts
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+import { Schema , InferSchemaType} from "mongoose";
+
+
+const UserSchema = new Schema({
+    sub: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    assets: [{
+        name: {
+            type: String,
+            required: true,
+        },
+        location: String,
+        credentials: String,
+        details: String,
+        notes: String,
+        activities: [{
+            name: {
+                type: String,
+                required: true,
+            },
+            todos: String,
+        }],
+        machines: [{
+            name: {
+                type: String,
+                required: true,
+            },
+            type: String,
+            unique_id: String,
+            service: String,
+            todos: String,
+        }],
+        inspections: [{
+            name: {
+                type: String,
+                required: true,
+            },
+            todos: String,
+            fisrtDate: {
+                type: Date,
+                required: true,
+            },
+            // fisrtDate: String,
+            // firstTime: String,
+            repeate_months: {
+                type: Number,
+                required: true,
+            },
+            repeate_type: {
+                type: String,
+                required: true,
+            },
+            google_calendar_task_id: {
+                type: String,
+                required: true,
+            },
+        }],
+    }],
+})
+
+export type UserType = InferSchemaType<typeof UserSchema>
+export const User = mongoose.model('User', UserSchema)
