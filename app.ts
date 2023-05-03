d610cd409dcaacf4399a02d5900207bd5795dd14
HdA <gaborneubauer.hda@gmail.com>
2023-05-03 19:24:26 +0200

    PUT /api/docs endpoint to separated route

--- app.ts
+++ app.ts
@@ -1,26 +1,15 @@
 import express, { Express } from 'express'
 import cors from 'cors'
-import swaggerJsdoc from "swagger-jsdoc"
-import swaggerUi from "swagger-ui-express"
 // import middlewares
 import { authenticateRequestMw } from './middlewares/authenticateRequest'
-// import data
-import openapi from './OpenAPI-doc_Landlord.json'
 // import routes
 import login from "./routes/login"
 import user from "./routes/user"
 import health from "./routes/health"
 import ready from "./routes/ready"
-// import docs from "./routes/docs"
+import docs from "./routes/docs"
 
 const app: Express = express()
-
-//configure serving api docs
-const options = {
-    apis: ["./routes/*.js"],
-    definition: openapi
-}
-const specs = swaggerJsdoc(options)
 
 // middleware functions
 app.use(cors())
@@ -32,7 +21,7 @@ app.use('/api/login', login)
 app.use('/api/user', user)
 app.use('/api/health', health)
 app.use('/api/ready', ready)
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }))
+app.use('/api/docs', docs)
 
 
 export default app
--- routes/docs.ts
+++ routes/docs.ts
@@ -1,28 +1,27 @@
-// import express, { Request, Response } from "express"
-// import swaggerJsdoc from "swagger-jsdoc"
-// import swaggerUi from "swagger-ui-express"
-// // import utility and middleware functions
-// import { filterMethodsMw } from "../middlewares/filterMethods"
-// // import data
-// import openapi from '../OpenAPI-doc_Landlord.json'
+import express, { Request, Response } from "express"
+import swaggerJsdoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+// import utility and middleware functions
+import { filterMethodsMw } from "../middlewares/filterMethods"
+// import data
+import openapi from '../OpenAPI-doc_Landlord.json'
 
-// // * router endpoint: /api/docs
-// const router = express.Router()
+// * router endpoint: /api/docs
+const router = express.Router()
 
-// // console.log(openapi.tags)
-// const options = {
-//     apis: ["./routes/*.js"],
-//     definition: openapi
-// }
-// const specs = swaggerJsdoc(options)
+//configure serving api docs
+const options = {
+    apis: ["../*.js"],
+    definition: openapi
+}
+const specs = swaggerJsdoc(options)
+
 
 // routes
-// router.all("/", filterMethodsMw(["GET"]))
+router.all("/", filterMethodsMw(["GET"]))
 
-// router.get("/", 
-//     swaggerUi.serve,
-//     swaggerUi.setup(specs, { explorer: true })
-// )
+router.use('/', swaggerUi.serve)
+router.get("/", swaggerUi.setup(specs, { explorer: true }))
 
 
-// export default router
+export default router
